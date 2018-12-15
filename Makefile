7b1f279cab9a82c908a7c5938bc32305df945ae9
Pavel Podkorytov <pod.pavel@gmail.com>
2018-12-16 01:12:06 +0500

    Add urls mapping

--- Makefile
+++ Makefile
@@ -7,6 +7,9 @@ run:
 print-empty-urls:
 	@docker run -it --rm -v $(PWD):/data idb-convert --print-empty-urls /data/IntenseDebate_clean.xml
 
+print-urls-mapping:
+	@docker run -it --rm -v $(PWD):/data --entrypoint /app/gen_new_urls.py idb-convert titles.json
+
 shell:
 	docker run -it --rm -v $(PWD):/data --entrypoint /bin/sh idb-convert
 
---
+++ gen_new_urls.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import json
+from datetime import datetime
+
+from lxml import etree, objectify
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("INPUT")
+    return parser.parse_args()
+
+
+def process_file(path):
+    with open("titles.json", "rb") as f:
+        titles = json.load(f)
+
+    result = []
+    for i in titles:
+        if "podcast" in i["categories"]:
+            num = extract_num(i["url"])
+            result.append((num, i["url"]))
+
+    return result
+
+
+def extract_num(url):
+    _, podcast, _ = url.rsplit("/", 2)
+    num = podcast.replace("podcast-", "")
+    return num
+
+
+def main():
+    args = parse_args()
+    result = process_file(args.INPUT)
+
+    for num, url in result:
+        print("http://radio-t.com/podcasts/radio-t-{num}/ -> {url}".format(num=num, url=url))
+
+
+if __name__ == "__main__":
+    main()
+
